0141ae3aeba31f339b4eb4d78b5940d9d3c2cd06
iredith <iredith0112@gmail.com>
2023-11-30 07:48:50 +0530

    [master]: added few more SQL queries to fetch, insert or delete in products table

--- 01-connecting-our-app/controllers/shop.js
+++ 01-connecting-our-app/controllers/shop.js
@@ -2,34 +2,40 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/product-list', {
-      prods: products,
-      pageTitle: 'All Products',
-      path: '/products'
-    });
-  });
+  Product.fetchAll()
+    .then(([ rows ]) => {
+      res.render('shop/product-list', {
+        prods: rows,
+        pageTitle: 'All Products',
+        path: '/products'
+      });
+    })
+    .catch(err => console.error(err));
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    res.render('shop/product-detail', {
-      product: product,
-      pageTitle: product.title,
-      path: '/products'
-    });
-  });
+  Product.findById(prodId)
+    .then(([ rows ]) => {
+      res.render('shop/product-detail', {
+        product: rows[0],
+        pageTitle: rows[0].title,
+        path: '/products'
+      });
+    })
+    .catch(err => console.error(err));
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/index', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/'
-    });
-  });
+  Product.fetchAll()
+    .then(([ rows ]) => {
+      res.render('shop/index', {
+        prods: rows,
+        pageTitle: 'Shop',
+        path: '/'
+      });
+    })
+    .catch(err => console.error(err));
 };
 
 exports.getCart = (req, res, next) => {
@@ -55,18 +61,22 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, product => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect('/cart');
+  Product.findById(prodId)
+    .then(([ rows ]) => {
+      Cart.addProduct(prodId, rows[0].price);
+      res.redirect('/cart');
+    })
+    .catch(err => console.error(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, product => {
-    Cart.deleteProduct(prodId, product.price);
-    res.redirect('/cart');
-  });
+  Product.findById(prodId)
+    .then(([ rows ]) => {
+      Cart.deleteProduct(prodId, rows[0].price);
+      res.redirect('/cart');
+    })
+    .catch(err => console.error(err));
 };
 
 exports.getOrders = (req, res, next) => {
